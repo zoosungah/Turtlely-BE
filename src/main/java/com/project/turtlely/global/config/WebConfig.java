87b86b903e4386960def580ea8ee7fcdd2ca29cf
SungahChoo <blackdog03@naver.com>
2026-05-14 00:36:26 +0900

    feat: CORS 설정 수정

--- src/main/java/com/project/turtlely/global/config/WebConfig.java
+++ src/main/java/com/project/turtlely/global/config/WebConfig.java
@@ -10,7 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 API 경로에 대해
-                .allowedOrigins("http://localhost:64600") // 프론트 로컬 주소 허용
+                .allowedOrigins("http://localhost:64600", // 프론트 기존 포트
+                                "http://localhost:3000", // 프론트 새 포트
+                                "http://54.144.66.35.nip.io:8080") // 백엔드 배포 주소
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS") // 허용할 HTTP 메서드
                 .allowedHeaders("*")
                 .allowCredentials(true)
