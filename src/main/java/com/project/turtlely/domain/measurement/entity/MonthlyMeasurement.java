c7b1e5e4cc41cba89e8f22ee26f6ba643d5fb410
SungahChoo <blackdog03@naver.com>
2026-05-27 21:58:41 +0900

    feat: 월간측정 엔티티 수정

--- src/main/java/com/project/turtlely/domain/measurement/entity/MonthlyMeasurement.java
+++ src/main/java/com/project/turtlely/domain/measurement/entity/MonthlyMeasurement.java
@@ -3,12 +3,15 @@ package com.project.turtlely.domain.measurement.entity;
 import com.project.turtlely.domain.member.entity.Member;
 import com.project.turtlely.global.entity.BaseEntity;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MonthlyMeasurement extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,4 +32,17 @@ public class MonthlyMeasurement extends BaseEntity {
 
     @Column(columnDefinition = "TEXT")
     private String predictionData;
+
+    // 하드웨어 데이터, 상수
+    private Float hwAccelX;
+    private Float hwAccelY;
+    private Float hwAccelZ;
+    private Float calibrationC;
+
+    public void updateHardwareCalibration(float hwAccelX, float hwAccelY, float hwAccelZ, float calibrationC) {
+        this.hwAccelX = hwAccelX;
+        this.hwAccelY = hwAccelY;
+        this.hwAccelZ = hwAccelZ;
+        this.calibrationC = calibrationC;
+    }
 }
